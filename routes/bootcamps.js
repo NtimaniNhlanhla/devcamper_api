496eb2bb3e2ead7254fa7a815bcc2857855572cd
Nhlanhla <nhlanhla@vitd.co>
2022-11-26 21:01:22 +0200

    Adding logger middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,4 +6,4 @@ const router = express.Router();
 router.route('/').get(getBootcamps).post(createBootcamp);
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 
-module.exports = router;
+module.exports = router; 
