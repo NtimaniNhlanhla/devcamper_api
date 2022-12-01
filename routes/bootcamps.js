9bc013331aaf0e27ae30480027f33c4eba1daeee
Nhlanhla <nsntimani@gmail.com>
2022-12-01 18:36:17 +0200

    Added Roles

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -7,21 +7,26 @@ const { getBootcamps,
     getBootcampsInRadius,
     bootcampPhotoUpload} = require('../controllers/bootcamps');
 
-const courseRouter = require('./courses'); 
+
 const Bootcamp = require('../models/Bootcamp');
 const advancedResults = require('../middleware/advancedResults');  
 
+// Includes other resource routes
+const courseRouter = require('./courses'); 
 const router = express.Router();
+
+const { protect, authorize } = require('../middleware/auth')
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
 
-router.route('/:id/photo').put(bootcampPhotoUpload)
+router.route('/:id/photo').put(protect,authorize('publisher', 'admin'),bootcampPhotoUpload)
 
 router.route('/')
        .get(advancedResults(Bootcamp, 'courses'),getBootcamps)
-       .post(createBootcamp);
-router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
+       .post(protect,authorize('publisher', 'admin'),createBootcamp);
+router.route('/:id').get(getBootcamp).put(protect,authorize('publisher', 'admin'),updateBootcamp)
+.delete(protect,authorize('publisher', 'admin'),deleteBootcamp)
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 
 module.exports = router; 
