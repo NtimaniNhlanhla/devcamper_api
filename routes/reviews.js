87b3787a94232b1910bd6f8d09b761f6466ed9c0
Nhlanhla <nsntimani@gmail.com>
2022-12-08 07:12:34 +0200

    update reviews router

--- routes/reviews.js
+++ routes/reviews.js
@@ -8,24 +8,21 @@ const {
 } = require('../controllers/reviews')
 
 const Review = require('../models/Review');
-const advancedResults = require('../middleware/advancedResults');      
+const advancedResults = require('../middleware/advancedResults');
 
 const { protect, authorize } = require('../middleware/auth')
 
-const router = express.Router({ mergeParams: true});
+const router = express.Router({ mergeParams: true });
 
-router.route('/').get(advancedResults(Review,{
+router.route('/').get(advancedResults(Review, {
     path: 'bootcamp',
     select: 'name description'
-}),getReviews)
-.post(protect,authorize('user', 'admin'),addReview);
+}), getReviews)
+    .post(protect, authorize('user', 'admin'), addReview);
 router.route('/:id').get(getReview)
-.put(protect,authorize('user', 'admin'),updateReview)
-.delete(protect,authorize('user', 'admin'),deleteReview);
+    .put(protect, authorize('user', 'admin'), updateReview)
 
-// router.route('/:id').get(getCourse)
-//                      .put(protect,authorize('publisher', 'admin'),updateCourse)
-//                      .delete(protect,authorize('publisher', 'admin'),deleteCourse);
+    .delete(protect, authorize('user', 'admin'), deleteReview);
+
 
 module.exports = router; 
-
